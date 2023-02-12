71b82cb940accc1e8968566f2759d367fbbedb29
Sart-Hack <sarthakgupta124@gmail.com>
2023-02-12 09:52:50 +0530

    Add firesbase config

---
+++ Firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAi-Olhb2rC1v-P16gYYeAf5cTjJmchvMo",
+    authDomain: "networkher-6bd68.firebaseapp.com",
+    projectId: "networkher-6bd68",
+    storageBucket: "networkher-6bd68.appspot.com",
+    messagingSenderId: "1045098596914",
+    appId: "1:1045098596914:web:6768d9736170e10f7c0a2b"
+  };
